9734ae5ceddcecc095ef0194dc1d59eed42e3b6e
taigaozawa <taigaozawa@gmail.com>
2021-04-12 19:01:12 +0900

    Docker対応

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:14.15.4
+
+RUN apt-get update
+RUN apt-get install -y locales vim tmux
+RUN locale-gen ja_JP.UTF-8
+RUN localedef -f UTF-8 -i ja_JP ja_JP
+RUN yarn global add htpasswd@2.4.4
+ENV LANG ja_JP.UTF-8
+ENV TZ Asia/Tokyo
+WORKDIR /app
--- test.js
+++ test.js
@@ -4,18 +4,19 @@ const assert = require('assert');
 
 // pug のテンプレートにおける XSS 脆弱性のテスト
 const html = pug.renderFile('./views/posts.pug', {
-  posts: [{
-    id: 1,
-    content: '<script>alert(\'test\');</script>',
-    postedBy: 'guest1',
-    trackingCookie: '2639292283224063_ddcc625203464a9e10af58fc3eb92eed7df4b9b5',
-    createdAt: new Date(),
-    updatedAt: new Date()
-  }],
+  posts: [
+    {
+      id: 1,
+      content: "<script>alert('test');</script>",
+      postedBy: 'guest1',
+      trackingCookie: '4391976947991005_0d6aeb0d6ad6bc82d29857339d6f304b3054dd5b',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  ],
   user: 'guest1'
 });
 
 // スクリプトタグがエスケープされて含まれていることをチェック
-assert(html.includes('&lt;script&gt;alert(\'test\');&lt;/script&gt;'));
+assert(html.includes("&lt;script&gt;alert('test');&lt;/script&gt;"));
 console.log('テストが正常に完了しました');
-
